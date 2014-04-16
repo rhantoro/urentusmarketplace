d924f5509ec7b9f570125486d678290bf878b9b5
rhantoro <rhantoro@gmail.com>
2014-04-16 15:39:14 +0700

    font

--- application/logs/log-2014-04-16.php
+++ application/logs/log-2014-04-16.php
@@ -3,3 +3,8 @@
 ERROR - 2014-04-16 10:21:44 --> 404 Page Not Found --> pages
 ERROR - 2014-04-16 10:32:17 --> Language file contains no data: language/english/english_lang.php
 ERROR - 2014-04-16 10:33:07 --> Language file contains no data: language/english/english_lang.php
+ERROR - 2014-04-16 10:35:54 --> Language file contains no data: language/english/english_lang.php
+ERROR - 2014-04-16 10:35:58 --> Language file contains no data: language/english/english_lang.php
+ERROR - 2014-04-16 10:36:53 --> Language file contains no data: language/english/english_lang.php
+ERROR - 2014-04-16 10:36:57 --> Language file contains no data: language/english/english_lang.php
+ERROR - 2014-04-16 10:38:10 --> Language file contains no data: language/english/english_lang.php
